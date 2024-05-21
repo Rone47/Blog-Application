f1e1d063e524ca35575db2c122d20f44a5fc9760
Rone47 <ryanbyrone47@gmail.com>
2024-05-21 21:05:30 +0300

    Footer complete

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,96 @@
+import { Footer } from "flowbite-react";
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+import { Link } from "react-router-dom";
+
+export default function FooterComp() {
+  return (
+    <Footer container className="border border-t-2 border-teal-800">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                World
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+            <Footer.Title tittle='About'/>
+            <Footer.LinkGroup col>
+                <Footer.Link 
+                   href="https://github.com/Rone47/worldblog"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >
+                    Blog
+                </Footer.Link>
+                <Footer.Link 
+                   href="/about"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >
+                    Services
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+            <div>
+            <Footer.Title tittle='Follow Us'/>
+            <Footer.LinkGroup col>
+                <Footer.Link 
+                   href="https://github.com/Rone47"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >
+                    Github
+                </Footer.Link>
+                <Footer.Link 
+                   href="/"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >
+                    Facebook
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+            <div>
+            <Footer.Title tittle='Legal Information'/>
+            <Footer.LinkGroup col>
+                <Footer.Link 
+                   href="#"
+                >
+                    Privacy Policy
+                </Footer.Link>
+                <Footer.Link 
+                   href="#"
+                >
+                    Terms of Service
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+        <Footer.Copyright
+            href='#'
+            by="WorldBlog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='#' icon={BsFacebook}/>
+            <Footer.Icon href='#' icon={BsInstagram}/>
+            <Footer.Icon href='#' icon={BsTwitter}/>
+            <Footer.Icon href='https://github.com/Rone47' icon={BsGithub}/>
+            <Footer.Icon href='#' icon={BsDribbble}/>
+
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
