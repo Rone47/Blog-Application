5fc72af9de184f5b563b149a0e8d8711a5025f35
Rone47 <ryanbyrone47@gmail.com>
2024-05-22 10:38:39 +0300

    addd

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 
 export default function FooterComp() {
   return (
-    <Footer container className="border border-t-2 border-teal-800">
+    <Footer container className="border border-t-2 border-slate-300">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
           <div className="mt-5">
