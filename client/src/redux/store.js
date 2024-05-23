cc183d8089fcf18f7d2b18ac383b141d7584634b
Rone47 <ryanbyrone47@gmail.com>
2024-05-23 16:41:51 +0300

    complete dark mode

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,16 +1,18 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import themeReducer from "./theme/themeSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer
 });
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  version: 1,
+  version: 1
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -18,7 +20,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
+    getDefaultMiddleware({ serializableCheck: false })
 });
 
 export const persistor = persistStore(store);
