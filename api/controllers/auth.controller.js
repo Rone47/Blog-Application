21d795d80ed025100c04642bebc5aa650da651d4
Rone47 <ryanbyrone47@gmail.com>
2024-05-27 20:14:56 +0300

    admin api functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -49,7 +49,7 @@ export const signin = async (req, res, next) => {
 
     const token = jwt.sign(
       {
-        id: validUser._id
+        id: validUser._id, isAdmin: validUser.isAdmin
       },
       process.env.JWT_SECRET,
       { expiresIn: "1d" }
@@ -73,7 +73,7 @@ export const google = async (req, res, next) => {
     if (user) {
       const token = jwt.sign(
         {
-          id: user._id
+          id: user._id, isAdmin: user.isAdmin
         },
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
@@ -101,7 +101,7 @@ export const google = async (req, res, next) => {
       await newUser.save();
       const token = await jwt.sign(
         {
-          id: newUser._id
+          id: newUser._id, isAdmin: newUser.isAdmin
         },
         process.env.JWT_SECRET
       );
