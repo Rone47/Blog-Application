247a2a9fa034e156f33916a69de32ca5f64d52d5
Rone47 <ryanbyrone47@gmail.com>
2024-06-12 20:11:42 +0300

    show comment section

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,34 +3,34 @@ import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
 
 export const test = (req, res) => {
-  res.json({ message: "API is working" });
+  res.json({ message: 'API is working!' });
 };
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, "You are not allowed to update this user"));
+    return next(errorHandler(403, 'You are not allowed to update this user'));
   }
   if (req.body.password) {
-    if (req.body.password.length < 8) {
-      return next(errorHandler(400, "Password must be at least 8 characters"));
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, 'Password must be at least 6 characters'));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
   if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
-        errorHandler(400, "Username must be between 7 and 20 characters")
+        errorHandler(400, 'Username must be between 7 and 20 characters')
       );
     }
-    if (req.body.username.includes(" ")) {
-      return next(errorHandler(400, "Username cannot contain spaces"));
+    if (req.body.username.includes(' ')) {
+      return next(errorHandler(400, 'Username cannot contain spaces'));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
-      return next(errorHandler(400, "Username must be lowercase"));
+      return next(errorHandler(400, 'Username must be lowercase'));
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
-        errorHandler(400, "Username can only contain letters and numbers")
+        errorHandler(400, 'Username can only contain letters and numbers')
       );
     }
   }
@@ -42,8 +42,8 @@ export const updateUser = async (req, res, next) => {
           username: req.body.username,
           email: req.body.email,
           profilePicture: req.body.profilePicture,
-          password: req.body.password
-        }
+          password: req.body.password,
+        },
       },
       { new: true }
     );
@@ -53,7 +53,6 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
-
 
 export const deleteUser = async (req, res, next) => {
   if (!req.user.isAdmin && req.user.id !== req.params.userId) {
@@ -115,6 +114,19 @@ export const getUsers = async (req, res, next) => {
       totalUsers,
       lastMonthUsers,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
